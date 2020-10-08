dd6882cf9638d5852d4550b4ef1d203a00f4a347
suarka <manksuarka@gmail.com>
2020-10-08 11:53:55 +0800

    add aplication router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+ BrowserRouter as Router,
+ Switch,
+ Route,
+} from "react-router-dom";
+import Home from './Home';
+import Register from './Register';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ return (
+  <Router basename='/ (repository)'>
+  <Switch>
+    <Route exact path='/'>
+      <Home/>
+    </Route>
+    <Route path='/register'>
+      <Register/>
+    </Route>
+  </Switch>
+</Router>
+ );
 }
 
 export default App;
